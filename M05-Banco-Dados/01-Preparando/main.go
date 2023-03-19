25865fdaedc9d97d5ce40d2a5f6e2212e52e6b06
Amaury Euzebio <amaury@a1tecnologia.info>
2023-03-19 11:22:14 -0300

    Banco de dados: update

--- M05-Banco-Dados/01-Preparando/main.go
+++ M05-Banco-Dados/01-Preparando/main.go
@@ -23,17 +23,19 @@ func NewProduct(name string, price float64) *Product {
 
 func main() {
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/m05")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
+	product := NewProduct("Notebook", 1899.00)
+	err = insertProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
 
-	defer db.Close()
-
-	product := NewProduct("Notebook", 1899.00)
-
-	err = insertProduct(db, product)
-
+	product.Price = 100.0
+	err = updateProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
@@ -41,15 +43,27 @@ func main() {
 
 func insertProduct(db *sql.DB, product *Product) error {
 	stmt, err := db.Prepare("insert into products(id, name, price) values (?, ?, ?)")
-
 	if err != nil {
 		panic(err)
 	}
-
 	defer stmt.Close()
 
 	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
 
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
 	if err != nil {
 		return err
 	}
