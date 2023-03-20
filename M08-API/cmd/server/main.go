42c9915206e6da92483d25a93f1d3111cc246211
Amaury Euzebio <amaury@a1tecnologia.info>
2023-03-19 22:33:33 -0300

    API: Handler de produto

--- M08-API/cmd/server/main.go
+++ M08-API/cmd/server/main.go
@@ -1,9 +1,75 @@
 package main
 
-import "github.com/amauryeuzebio/goexpert/M08-API/configs"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/amauryeuzebio/goexpert/M08-API/configs"
+	"github.com/amauryeuzebio/goexpert/M08-API/internal/dto"
+	"github.com/amauryeuzebio/goexpert/M08-API/internal/entity"
+	"github.com/amauryeuzebio/goexpert/M08-API/internal/infra/database"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
 
-	println(config.DBDriver)
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":3000", nil)
+
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	p, err := entity.NewProduct(product.Name, product.Price)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	err = h.ProductDB.Create(p)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
--- M08-API/readme.md
+++ M08-API/readme.md
@@ -3,3 +3,9 @@
 ```bash
 go mod tidy
 ```
+
+### SQLite
+```bash
+# Acessar banco sqlite
+sqlite3 M08-API/cmd/server/test.db
+```
