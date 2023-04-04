80c6998d4d106341216417fe97b7570c33f38bff
Amaury Euzebio <amaury@a1tecnologia.info>
2023-04-03 21:33:11 -0300

    API: Exemplo de middleware

--- M08-API/cmd/server/main.go
+++ M08-API/cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/amauryeuzebio/goexpert/M08-API/configs"
@@ -9,7 +10,6 @@ import (
 	"github.com/amauryeuzebio/goexpert/M08-API/internal/infra/webserver/handlers"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -35,7 +35,8 @@ func main() {
 	userHandler := handlers.NewUserHandler(userDB, configs.TokenAuth, configs.JwtExperesIn)
 
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	// r.Use(middleware.Logger) // Comentado para exemplo de midware
+	r.Use(LogRequest)
 
 	r.Route("/products", func(r chi.Router) {
 		r.Use(jwtauth.Verifier(configs.TokenAuth))
@@ -51,5 +52,12 @@ func main() {
 	r.Post("/users/generate_token", userHandler.GetJWT)
 
 	http.ListenAndServe(":3000", r)
+}
 
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request: %s %s", r.Method, r.URL.Path)
+
+		next.ServeHTTP(w, r)
+	})
 }
