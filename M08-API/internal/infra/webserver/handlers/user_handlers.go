ee29e778f02c5c006ace4161441910c97ba2d725
Amaury Euzebio <amaury@a1tecnologia.info>
2023-04-03 22:37:02 -0300

    API: Documentação autenticação

--- M08-API/internal/infra/webserver/handlers/user_handlers.go
+++ M08-API/internal/infra/webserver/handlers/user_handlers.go
@@ -27,6 +27,17 @@ func NewUserHandler(db database.UserInterface) *UserHandler {
 	}
 }
 
+// GetJWT godoc
+// @Summary      Get a user JWT
+// @Description  Get a user JWT
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        request   body     dto.GetJWTInput  true  "user credentials"
+// @Success      200  {object}  dto.GetJWTOutput
+// @Failure      404  {object}  Error
+// @Failure      500  {object}  Error
+// @Router       /users/generate_token [post]
 func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
 	jwtExperiesIn := r.Context().Value("JwtExperesIn").(int)
@@ -42,7 +53,9 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	u, err := h.UserDB.FindByEmail(user.Email)
 
 	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
+		w.WriteHeader(http.StatusNotFound)
+		err := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(err)
 		return
 	}
 
@@ -56,11 +69,7 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 		"exp": time.Now().Add(time.Second * time.Duration(jwtExperiesIn)).Unix(),
 	})
 
-	accessToken := struct {
-		AccessToken string `json:"token"`
-	}{
-		AccessToken: tokenString,
-	}
+	accessToken := dto.GetJWTOutput{AccessToken: tokenString}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
