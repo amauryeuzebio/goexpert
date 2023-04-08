9f6de7aef8505eab559ceae599bbae2d94494487
Amaury Euzebio <amaury@a1tecnologia.info>
2023-04-08 20:13:59 -0300

    SQLC: Finalizando CRUD

--- M16-SQLC/cmd/runSQLC/main.go
+++ M16-SQLC/cmd/runSQLC/main.go
@@ -1,4 +1,59 @@
 package main
 
+import (
+	"context"
+	"database/sql"
+
+	"github.com/amauryeuzebio/goexpert/M16-SQLC/internal/db"
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
+	ctx := context.Background()
+	dbConn, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/courses")
+	if err != nil {
+		panic(err)
+	}
+	defer dbConn.Close()
+
+	queries := db.New(dbConn)
+
+	// err = queries.CreateCategory(ctx, db.CreateCategoryParams{
+	// 	ID:          uuid.New().String(),
+	// 	Name:        "Backend",
+	// 	Description: sql.NullString{String: "Backend description", Valid: true},
+	// })
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// categories, err := queries.ListCategories(ctx)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for _, category := range categories {
+	// 	println(category.ID, category.Name, category.Description.String)
+	// }
+
+	// err = queries.UpdateCategory(ctx, db.UpdateCategoryParams{
+	// 	ID:          "299c0752-5092-4abd-825f-934a43056dcb",
+	// 	Name:        "Backend updated",
+	// 	Description: sql.NullString{String: "Backend description updated", Valid: true},
+	// })
+
+	err = queries.DeleteCategory(ctx, "299c0752-5092-4abd-825f-934a43056dcb")
+	if err != nil {
+		panic(err)
+	}
+
+	categories, err := queries.ListCategories(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, category := range categories {
+		println(category.ID, category.Name, category.Description.String)
+	}
 }
--- M16-SQLC/go.mod
+++ M16-SQLC/go.mod
@@ -1,3 +1,8 @@
 module github.com/amauryeuzebio/goexpert/M16-SQLC
 
 go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/google/uuid v1.3.0
+)
